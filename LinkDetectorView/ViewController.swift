2068b3b6f9c72094bca757b4a8e96cae55d1a913
Yoshikuni Kato <y.kato@ohako-inc.jp>
2016-06-06 13:16:17 +0900

    Initial Commit

---
+++ LinkDetectorView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkDetectorView
+//
+//  Created by Yoshikuni Kato on 2016/06/06.
+//  Copyright © 2016年 Ohako Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
