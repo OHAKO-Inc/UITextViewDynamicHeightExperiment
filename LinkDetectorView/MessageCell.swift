51a55336686b4f2393c7805a6aef4d76eb90f7cd
Yoshikuni Kato <y.kato@ohako-inc.jp>
2016-06-06 16:28:28 +0900

    Implemented using "sizeThatFits" method (still have unknown margin)

---
+++ LinkDetectorView/MessageCell.swift
@@ -0,0 +1,49 @@
+//
+//  MessageCell.swift
+//  LinkDetectorView
+//
+//  Created by Yoshikuni Kato on 2016/06/06.
+//  Copyright © 2016年 Ohako Inc. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    static var dummyTextView = UITextView() {
+        didSet {
+            dummyTextView.textContainer.lineFragmentPadding = 0.0
+            dummyTextView.textContainerInset = UIEdgeInsetsZero
+            dummyTextView.showsVerticalScrollIndicator = false
+            dummyTextView.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    class func height(for message: String, width: CGFloat) -> CGFloat {
+        dummyTextView.font = UIFont(name: "Helvetica", size: 14.0)
+        dummyTextView.text = message
+        let size = dummyTextView.sizeThatFits(CGSize(width: width, height: CGFloat.max))
+        return size.height
+    }
+    
+    @IBOutlet weak var messageTextView: UITextView! {
+        didSet {
+            messageTextView.textContainer.lineFragmentPadding = 0.0
+            messageTextView.textContainerInset = UIEdgeInsetsZero
+        }
+    }
+    
+    func configure(with message: String) {
+        messageTextView.text = message
+        //        updateFrame(messageTextView)
+    }
+    
+    private func updateFrame(textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.max))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.max))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame;
+    }
+}
--- LinkDetectorView/ViewController.swift
+++ LinkDetectorView/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    lazy var messages: [String] = {
+        
+        var messages = [String]()
+
+        let random = GKRandomDistribution(lowestValue: 1, highestValue: 500)
+        
+        for line in 0...999 {
+            var message = ""
+
+                //            for i in 0...random.nextInt()
+            for i in 0...line {
+                message += "a"
+            }
+            message += "\(message.characters.count)"
+            messages.append(message)
+        }
+        
+        return messages
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //        tableView.rowHeight = UITableViewAutomaticDimension
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(String(MessageCell), forIndexPath: indexPath) as! MessageCell
+        cell.configure(with: messages[indexPath.row])
+        return cell
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let height = MessageCell.height(for: messages[indexPath.row], width: view.frame.width)
+        print(height)
+        return height
+    }
+    //    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    //        return UITableViewAutomaticDimension
+    //    }
+}
